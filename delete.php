4127338fc39599e2de02ac47ffdc650d5c1ab66f
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-01-13 11:17:31 +0100

    Update dsn

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 
 // https://stackoverflow.com/questions/30074492/what-is-the-difference-between-utf8mb4-and-utf8-charsets-in-mysql
-$dsn = 'mysql:host=localhost;dbname=php-pdo-crud-proeftoets;charset=utf8mb4';
+$dsn = 'mysql:host=localhost;dbname=php-pdo-crud-toets;charset=utf8mb4';
 
 $options = [
     // Default is FETCH_BOTH (both indexed and associative array)
